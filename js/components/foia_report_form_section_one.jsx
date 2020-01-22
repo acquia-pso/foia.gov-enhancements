e2535fbfc42672ee419aac622272d8746602031d
Tyler Fromma <tyler.fromma@gmail.com>
2020-01-22 09:17:30 -0500

    FOIA=264: Style section 1 fixed checkbox functions.

--- js/components/foia_report_form_section_one.jsx
+++ js/components/foia_report_form_section_one.jsx
@@ -18,18 +18,18 @@ class FoiaReportFormSectionOne extends React.Component {
               <FoiaTooltip text="Select the type of FOIA data you would like to view. The data comes from agencies' Annual FOIA Reports. To learn more about the data, view the terms in the Glossary." />
             </legend>
             <div className="form-group field">
-              <lable htmlFor="data_type"><b>Agency or Component Name</b></lable>
-               <span className="twitter-typeahead usa-search-bg-light usa-reset-width">
-                <input type="text"
-                     id="search-field-big"
-                     name="search"
-                     placeholder=""
-                     className=" usa-reset-width"
-                     autocomplete="off"
-                     spellcheck="false"
-                     dir="auto" />
-                <button className="usa-button usa-button-outline usa-button-small" type="submit" href="#">Select Agency Components</button>
-               </span>
+              <label htmlFor="data_type"><strong>Agency or Component Name</strong></label>
+                <span className="twitter-typeahead usa-search-bg-light usa-reset-width">
+                  <input type="text"
+                    id="search-field-big"
+                    name="search"
+                    placeholder=""
+                    className=" usa-reset-width"
+                    autocomplete="off"
+                    spellcheck="false"
+                    dir="auto" />
+                  <button className="usa-button usa-button-outline usa-button-small" type="submit" href="#">Select Agency Components</button>
+                </span>
             </div>
             <div className="form-group field use-dark-icons usa-grid">
               <a href="#"><span className="icon-plus"></span><p className="usa-alert-text">Add Another Agency or Component</p></a>
@@ -37,40 +37,41 @@ class FoiaReportFormSectionOne extends React.Component {
           </fieldset>
         </div>
         <div className="clear">&nbsp;</div>
-
-        <div className="form-group usa-width-one-half visually-hidden">
-          {/* Begin Modal Inner HTML */}
-          <div className="form-group field">
-            <h3 className="sans">Select Agencies or Components</h3>
-            <div className="usa-grid">
-            <div id="usa-unstyled-list">
-             <fieldset classNam="usa-fieldset-inputs usa-sans">
-       <ul className="usa-unstyled-list">
-         <li className="usa-width-one-third">
-           <input id="Agency 1" type="checkbox" name="Agency 1" checked />
-           <label for="agency 1">Agency 1</label>
-         </li>
-         <li className="usa-width-one-third">
-           <input id="Agency 2" type="checkbox" name="Agency 2" />
-           <label for="agency 2">Agency 2</label>
-         </li>
-         <li className="usa-width-one-third">
-           <input id="Agency 3" type="checkbox" name="Agency 3"  />
-           <label for="agency 3">Agency 3</label>
-         </li>
-       </ul>
-             </fieldset>
-            <div className="clear">&nbsp;</div>
-              <p className="usa-alert-text"><a href="#"><span>Select All<span /></span></a>  |  <a href="#"><span>Select None<span /></span></a></p>
-              <button className="usa-button usa-button-primary-alt usa-button-small" type="submit" href="#"><b>Submit</b></button>
-              <button className="usa-button usa-button-outline usa-button-small" type="submit" href="#"><b>Cancel</b></button>
+          <div className="form-group usa-width-one-half">
+            {/* Begin Modal Inner HTML */}
+            <div className="form-group field">
+              <h3 className="sans">Select Agencies or Components</h3>
+              <div className="usa-grid">
+                <fieldset className="usa-fieldset-inputs">
+                  <ul className="usa-unstyled-list usa-grid checkbox-list">
+                    <li className="usa-width-one-third">
+                      <input id="Agency-1" type="checkbox" name="Agency-1" value="Agency-1" />
+                      <label htmlFor="Agency-1">Agency 1</label>
+                    </li>
+                    <li className="usa-width-one-third">
+                      <input id="Agency-2" type="checkbox" name="Agency-2" value="Agency-2" />
+                      <label htmlFor="Agency-2">Agency 2</label>
+                    </li>
+                    <li className="usa-width-one-third">
+                      <input id="Agency-3" type="checkbox" name="Agency-3" value="Agency-3" />
+                      <label htmlFor="Agency-3">Agency 3</label>
+                    </li>
+                  </ul>
+                  <div className="form-group">
+                    <ul className="inline-list">
+                      <li><a href="#"><p>Select All</p></a></li> |
+                      <li><a href="#"><p>Select None</p></a></li>
+                    </ul>
+                  </div>
+                    <button className="usa-button usa-button-primary-alt usa-button-small" type="submit">Submit</button>
+                    <button className="usa-button usa-button-outline usa-button-small" type="submit">Cancel</button>
+                </fieldset>
+                </div>
             </div>
-            </div>
-             {/* End Modal Inner HTML */}
+                {/* End Modal Inner HTML */}
           </div>
-        </div>
       </div>
-    );
-   }
-}
+     );
+    }
+  }
 export default FoiaReportFormSectionOne;
